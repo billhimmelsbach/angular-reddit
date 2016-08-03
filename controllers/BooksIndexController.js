f3b46a9407882532e516f5601082e9edaa363d11
mehgellan <baumegg@gmail.com>
2016-08-03 14:02:01 -0700

    update input from MEG

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,14 +4,14 @@ angular.module('libraryApp')
 
 BooksIndexController.$inject=['$http'];
 
-  function BooksIndexController ($http) {
-    var vm = this;
-    vm.embedIdList = [];
-    console.log(vm.embedIdList);
-    vm.newBook = {};
-    vm.orderBy="title";
-    vm.reverse=false;
-    $http({
+function BooksIndexController ($http) {
+  var vm = this;
+  vm.embedIdList = [];
+  console.log(vm.embedIdList);
+  vm.newBook = {};
+  vm.orderBy="title";
+  vm.reverse=false;
+  $http({
     method: 'GET',
     url:'https://www.reddit.com/r/music/new.json?sort=new',
   }).then(function successCallback(response) {
@@ -33,101 +33,11 @@ BooksIndexController.$inject=['$http'];
         vm.embedIdList.push(embedId);
         // console.log(vm.embedIdList);
       }
-      // }
-    });
-
-    // for (var i = 0; i < redditPosts.length; i++) {
-    //   redditPosts[i].data.children;
     // }
-    vm.books = vm.embedIdList;
-    console.log(vm.books);
-    // for (var i = 0; i < redditPosts.length; i++) {
-    //   array[i]
-    // }
-    //   console.log(response.data.data.children.media_embed.content);
-    // }
-
-    // for (var i = 0; i < response.data.data.children; i++) {
-    //   url = response.data.data.children[i].data.url = url;
-    //   console.log(url);
-    //   if(preg_match("/youtu.be\/[a-z1-9.-_]+/", url)) {
-    //     preg_match("/youtu.be\/([a-z1-9.-_]+)/", url, matches);
-    //     if(isset(matches[1])) {
-    //         url = 'http://www.youtube.com/embed/'.matches[1];
-    //     }
-    //   }
-    //   else if(preg_match("/youtube.com(.+)v=([^&]+)/", url)) {
-    //     preg_match("/v=([^&]+)/", url, matches);
-    //     if(isset(matches[1])) {
-    //         url = 'http://www.youtube.com/embed/'.matches[1];
-    //     }
-    //   }
-    //   // Vimeo video
-    //   else if(preg_match("/vimeo.com\/[1-9.-_]+/", url)) {
-    //     preg_match("/vimeo.com\/([1-9.-_]+)/", url, matches);
-    //     if(isset(matches[1])) {
-    //         url = 'http://player.vimeo.com/video/'.matches[1];
-    //     }
-    //   }
-    //   console.log(url);
-    //   vm.books.push(url);
-    // }
-
-    // vm.books = response.data.data.children[i]
-// YouTube url?
-
-// return url;
-    // console.log(vm.books);
-    vm.books = response.data.data.children;
-    // console.log(response.data.data.children);
-    // console.log(response.data.data.children.data.url);
-    // console.log(vm.books);
-    // console.log(response.data.books)
-  }, function errorCallback(response) {
-    console.log('There was an error getting the data', response);
   });
-  //
-  // vm.createBook = function () {
-  //   console.log("test");
-  //   console.log(vm.newBook);
-  //   $http({
-  //     method: 'POST',
-  //     url: 'https://super-crud.herokuapp.com/books/',
-  //     data: vm.newBook,
-  //   }).then(function successCallback(response) {
-  //     vm.books.push(response.data);
-  //     console.log(response.data);
-  //     $('#myModal').modal('toggle');
-  //     // bookSound(bulbasaur);
-  //     setTimeout(function(){ $('.newBookForm').find("input[type=text], input[type=url], textarea").val(""); }, 3000);
-  //   }, function errorCallback(response) {
-  //     console.log('There was an error posting the data', response);
-  //   });
-  // };
-  // //
-  // // vm.editBook = function (book) {
-  // //   console.log(book);
-  // //   $http({
-  // //     method: 'PUT',
-  // //     url: 'https://super-crud.herokuapp.com/books/'+book._id,
-  // //     data: book
-  // //   }).then(function successCallback(json) {
-  // //     console.log("PUT!");
-  // //   }, function errorCallback(response) {
-  // //     console.log('There was an error editing the data', response);
-  // //   });
-  // // };
-  // //
-  // // vm.deleteBook = function (book) {
-  // //   $http({
-  // //     method: 'DELETE',
-  // //     url: 'https://super-crud.herokuapp.com/books/'+ book._id
-  // //   }).then(function successCallback(json) {
-  // //     console.log("test");
-  // //     var index = vm.book.indexOf(book);
-  // //     vm.books.splice(index,1);
-  // //   }, function errorCallback(response) {
-  // //     console.log('There was an error deleting the data', response);
-  // //   });
-  // // };
+
+  vm.books = vm.embedIdList;
+  console.log(vm.books);
+}
+
 }
