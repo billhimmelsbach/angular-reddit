47d0b5affbf0a84fd8e38ce5a9d5acf0ee90ba0f
mehgellan <baumegg@gmail.com>
2016-08-03 15:54:26 -0700

    add title with concatenation substring analysis

--- controllers/VideosIndexController.js
+++ controllers/VideosIndexController.js
@@ -6,8 +6,7 @@ VideosIndexController.$inject=['$http'];
 
 function VideosIndexController ($http) {
   var vm = this;
-  vm.embedIdList = [];
-  console.log(vm.embedIdList);
+  vm.dataSet = [];
   // vm.orderBy="title";
   // vm.reverse=false;
   $http({
@@ -17,9 +16,14 @@ function VideosIndexController ($http) {
     // console.log(response.data.data.children);
     redditPosts = response.data.data.children;
     redditPosts.forEach(function (post) {
+      var videoTitle = [];
       if (post.data.secure_media !== null) {
-        var videoTitle = post.data.secure_media.oembed.title;
+        videoTitle = post.data.secure_media.oembed.title;
         console.log(videoTitle);
+        if (videoTitle.length > 25) {
+          videoTitle = videoTitle.substring(0, 25);
+          videoTitle = videoTitle + '...';
+        }
       }
       var video_id = post.data.url.split('v=')[1];
       if (video_id !== undefined) {
@@ -28,11 +32,16 @@ function VideosIndexController ($http) {
           video_id = video_id.substring(0, ampersandPosition);
         }
         var embedId= "http://www.youtube.com/embed/" + video_id;
-        vm.embedIdList.push(embedId);
+        if (videoTitle !== []) {
+          vm.dataSet.push({
+            url: embedId,
+            title: videoTitle
+          });
+        }
       }
+      console.log(vm.dataSet);
     });
-    vm.videos = vm.embedIdList;
-    console.log(vm.videos);
+    console.log(vm.dataSet.url);
 
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
